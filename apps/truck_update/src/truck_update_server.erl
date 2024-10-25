5525dc02a0851db3240e41f6da0dd097d27a28ea
swartsd <82994049+swartsd@users.noreply.github.com>
2024-10-25 11:50:29 -0600

    Added truck update

---
+++ apps/truck_update/src/truck_update_server.erl
@@ -0,0 +1,31 @@
+%% truck_update.erl
+-module(truck_update_server).
+-behaviour(gen_server).
+
+-export([start_link/0, update_location/3]).
+-export([init/1, handle_call/3, terminate/2]).
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+update_location(TruckID, Lat, Long) ->
+    gen_server:call(?MODULE, {update, TruckID, Lat, Long}).
+
+init([]) ->
+    {ok, #{}}.
+
+handle_call({update, TruckID, Lat, Long}, _From, State) ->
+    %% Simulate interaction with db_client here
+    case db_client:find_truck(TruckID) of
+        {ok, Truck} ->
+            UpdatedTruck = Truck#{latitude => Lat, longitude => Long},
+            ok = db_client:update_truck(UpdatedTruck),
+            {reply, {ok, updated}, State};
+        error ->
+            NewTruck = #{id => TruckID, latitude => Lat, longitude => Long},
+            ok = db_client:insert_truck(NewTruck),
+            {reply, {ok, inserted}, State}
+    end.
+
+terminate(_Reason, _State) ->
+    ok.
