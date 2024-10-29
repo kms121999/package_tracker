75d06eaa9e266f24d7ae01cbd36a3b283783bbaf
Keaton Smith <keaton.smith365@gmail.com>
2024-10-29 14:01:22 -0600

    Added package_update endpoint

--- apps/package_tracker/src/package_tracker_app.erl
+++ apps/package_tracker/src/package_tracker_app.erl
@@ -13,8 +13,8 @@ start(_StartType, _StartArgs) ->
     Dispatch = cowboy_router:compile([
         {'_', [
             {"/package/:package_id", package_get_handler, []},
-            {"/truck", truck_update_handler, []}
-            % {"package/update/:package_id", package_update_handler, []}
+            {"/truck", truck_update_handler, []},
+            {"/package/update/:package_id", package_update_handler, []}
         ]}
         % {"packages.localhost", [{"/:package_id", package_get_handler, []}]}
     ]),
