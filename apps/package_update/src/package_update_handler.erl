e01b1102d87cdfee35d44ff4ac74aaf1d025ff52
Gerin Wilde <91751688+GerinWilde1@users.noreply.github.com>
2024-11-04 09:38:43 -0700

    PackUpServer

--- apps/package_update/src/package_update_handler.erl
+++ apps/package_update/src/package_update_handler.erl
@@ -38,11 +38,6 @@ terminate(_Reason, _Req, _State) ->
 
 
 
-%%% Only include the eunit testing library and functions
-%%% in the compiled code if testing is 
-%%% being done.
--ifdef(TEST).
--include_lib("eunit/include/eunit.hrl").
+
  
 
--endif.
--- apps/package_update/src/package_update_server.erl
+++ apps/package_update/src/package_update_server.erl
@@ -2,33 +2,123 @@
 -module(package_update_server).
 -behavior(gen_server).
 
--export([start_link/0, update_location/3]).
+-export([start_link/0, update_package/2]).
 
 -export([init/1, handle_call/3, terminate/2]).
 
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
-update_location(packageID, Lat, Long) ->
-    gen_server:call(?MODULE, {update, packageID, Lat, Long}).
+update_package(PackageID, Package_data) ->
+    gen_server:call(?MODULE, {update, PackageID, Package_data}).
 
 init([]) ->
     {ok, #{}}.
 
-handle_call({update, PackageID, Lat, Long}, _From, State) ->
+handle_call({update, PackageID, Package_data}, _From, State) ->
     %% Simulate interaction with db_client here
-    case db_client:find_package(PackageID) of
-        {ok, Package} ->
-            UpdatedPackage = Package#{latitude => Lat, longitude => Long},
-            ok = db_client:update_package(UpdatedPackage),
-            {reply, {ok, updated}, State};
-        error ->
-            NewPackage = #{id => PackageID, latitude => Lat, longitude => Long},
-            ok = db_client:package(NewPackage),
+    case database_client:get(State, <<"packages">>, PackageID) of
+        {ok, _Package} ->
+            
+            case database_client:put(State, <<"packages">>, PackageID, Package_data) of
+                {ok, _Updated} ->
+                    {reply, {ok, updated}, State};
+                {error, _Reason} ->
+                    {reply, {error, database_error}, State}
+                % {reply, {ok, updated}, State}
+                end;
+            
+        {error, not_found} ->
+            ok = database_client:put(State, <<"packages">>, PackageID, Package_data),
             {reply, {ok, inserted}, State}
     end.
+
+
+    
 
 terminate(_Reason, Connection) ->
     %% Close the database connection
     database_client:disconnect(Connection),
     ok.
+
+
+%% Import EUnit and Meck for testing and mocking
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+package_update_test_() ->
+    {setup,
+        fun setup/0,
+        fun cleanup/1,
+        [
+            fun test_package_found/0
+        ]
+    }.
+
+setup() ->
+    meck:new(database_client),
+
+    meck:expect(database_client, connect, 0, {ok, mock_connection}),
+    
+    meck:expect(database_client, disconnect, 1, ok),
+
+        %% Start the package_get_server service
+        {ok, Pid} = package_update_server:start_link(),
+    
+        %% Return the Pid to use in cleanup
+        Pid.
+
+cleanup(Pid) ->
+    gen_server:stop(Pid),
+    
+    %% Unload the meck mock for database_client
+    meck:unload(database_client).
+
+
+test_package_found()->
+    Package123 = <<"package123">>,
+    StoredPackageData = #{
+        <<"sender">> => <<"Alice">>, 
+        <<"receiver">> => <<"Bob">>, 
+        <<"destination">> => 
+            #{ <<"street">> => <<"123 Cat Lane">>, 
+                <<"city">> => <<"Wonderland">>, 
+                <<"state">> => <<"NY">>, 
+                <<"zip">> => <<"12345">>, 
+                <<"country">> => <<"USA">> }, 
+        <<"returnAddress">> => 
+            #{ <<"street">> => <<"456 Yellow Brick Rd">>, 
+                <<"city">> => <<"OZ">>, 
+                <<"state">> => <<"KS">>, 
+                <<"zip">> => <<"54321">>, 
+                <<"country">> => <<"England">> }, 
+        <<"status">> => <<"in transit">>, 
+        <<"priority">> => <<"overnight">>, 
+        <<"truckId">> => <<"truck123">>
+    },
+
+    meck:expect(database_client, get, 3, 
+        fun (_Connection, <<"packages">>, <<"package123">>) ->
+                {ok, StoredPackageData};
+            (_Connection, <<"packages">>, <<"bad_package">>) ->
+                {error, not_found};
+            (_Connection, <<"packages">>, <<"databasedown">>) ->
+                {error, "Database down"}
+
+        end
+	),
+
+    meck:expect(database_client, put, 4, 
+        fun (_Connection, <<"packages">>, <<"package123">>, StoredData) ->
+                {ok, StoredPackageData};
+            (_Connection, <<"packages">>, <<"bad_package">>, StoredData) ->
+                {error, StoredPackageData};
+            (_Connection, <<"packages">>, <<"databasedown">>, StoredData) ->
+                {error, "Database down"}
+
+        end
+	),
+
+    ?assertEqual(ok, package_update_server:update_package(Package123, StoredPackageData)).
+
+-endif.
