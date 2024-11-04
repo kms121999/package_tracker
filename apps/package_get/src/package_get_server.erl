375a29f7a8627e84c5b7d8893395ed00aa7ba885
Keaton Smith <keaton.smith365@gmail.com>
2024-11-04 12:05:28 -0700

    Fixed export

--- apps/package_get/src/package_get_server.erl
+++ apps/package_get/src/package_get_server.erl
@@ -4,7 +4,7 @@
 -behavior(gen_server).
 
 %% API
--export([start_link/0, get_package_data/1]).
+-export([start_link/0, get_package_data/2]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, terminate/2]).
