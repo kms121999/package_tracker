73cb37a8e533108498df799e73f3a941de5b1c74
Keaton Smith <keaton.smith365@gmail.com>
2024-11-06 16:05:30 -0700

    Updated log file

--- apps/lumberjack/src/lumberjack_server.erl
+++ apps/lumberjack/src/lumberjack_server.erl
@@ -41,7 +41,7 @@ init([]) ->
     %% Start the event manager to handle log events
     {ok, EventManager} = gen_event:start_link(),
     gen_event:add_handler(EventManager, lumberjack_console_handler, []),
-    gen_event:add_handler(EventManager, lumberjack_file_handler, []),
+    gen_event:add_handler(EventManager, lumberjack_file_handler, ["./lumberjack.log"]),
     {ok, EventManager}.
 
 %% Handle synchronous calls
