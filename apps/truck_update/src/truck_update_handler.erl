e8a0ab8cb932c7b2e0b5680ef049a12e9c14c6c8
Keaton Smith <keaton.smith365@gmail.com>
2024-11-19 13:52:01 -0700

    Updated logs truck update handler

--- apps/truck_update/src/truck_update_handler.erl
+++ apps/truck_update/src/truck_update_handler.erl
@@ -11,26 +11,29 @@ init(Req=#{method := <<"POST">>}, State) ->
     {ok, Body, Req1} = cowboy_req:read_body(Req),
     ParsedData = jiffy:decode(Body, [return_maps]),
 
-
     %% Extract values
-    TruckID = maps:get(<<"truckId">>, ParsedData),
+    TruckId = maps:get(<<"truckId">>, ParsedData),
     Lat = maps:get(<<"lat">>, maps:get(<<"location">>, ParsedData)),
     Long = maps:get(<<"long">>, maps:get(<<"location">>, ParsedData)),
 
+    lumberjack_server:info("Received truck update request", #{module => ?MODULE, truckId => TruckId, peer_ip => cowboy_req:peer(Req1), req_id => Req_id}),
+
     %% Call the truck_update server
-    Result = truck_update_server:update_location(TruckID, Lat, Long),
+    truck_update_server:update_location(TruckId, Lat, Long),
+
+    lumberjack_server:info("Truck location update triggered", #{module => ?MODULE, truckId => TruckId, req_id => Req_id}),
 
     %% Prepare and send response
-    Response = case Result of
-        ok -> {200, #{status => noreply}};
-        {error, Reason} -> {500, #{error => Reason}}
-    end,
-    {StatusCode, RespBody} = Response,
+    {StatusCode, RespBody} = {202, #{status => noreply}},
     Req2 = cowboy_req:reply(StatusCode, #{<<"content-type">> => <<"application/json">>}, jiffy:encode(RespBody), Req1),
     {ok, Req2, State};
 
 
 init(Req0, State) ->
+    Req_id = maps:get(req_id, maps:get(package_tracker, Req0)),
+
+    lumberjack_server:warning("Invalid request method", #{module => ?MODULE, method => cowboy_req:method(Req0), peer_ip => cowboy_req:peer(Req0), req_id => Req_id}),
+    
     Req1 = cowboy_req:reply(405, #{
         <<"allow">> => <<"POST">>
     }, Req0),
