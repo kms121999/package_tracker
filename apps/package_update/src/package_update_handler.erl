247cf3fe7fd14cb5edc645e05957b0ee10d39b8a
Keaton Smith <keaton.smith365@gmail.com>
2024-11-19 13:45:06 -0700

    Updated logs for package_update

--- apps/package_update/src/package_update_handler.erl
+++ apps/package_update/src/package_update_handler.erl
@@ -12,16 +12,23 @@ init(Req=#{method := <<"POST">>}, State) ->
     ParsedData = jiffy:decode(Body, [return_maps]),
 
     %% Extract values
-    PackageID = maps:get(<<"packageId">>, ParsedData),
-    Data = maps:remove(PackageID, ParsedData),
+    PackageId = maps:get(<<"packageId">>, ParsedData),
+    Data = maps:remove(PackageId, ParsedData),
+
+    lumberjack_server:info("Received package update request", #{module => ?MODULE, packageId => PackageId, peer_ip => cowboy_req:peer(Req1), req_id => Req_id}),
+
     
     %% Call the package_update server
-    Result = package_update_server:update_package(PackageID, Data),
+    Result = package_update_server:update_package(PackageId, Data),
 
     %% Prepare and send response
     Response = case Result of
-        {ok, Status} -> {200, #{status => Status}};
-        {error, Reason} -> {500, #{error => Reason}}
+        {ok, Status} ->
+            lumberjack_server:info("Package updated", #{module => ?MODULE, packageId => PackageId, req_id => Req_id, status => Status}),
+            {200, #{status => Status}};
+        {error, Reason} ->
+            lumberjack_server:error("Package update failed", #{module => ?MODULE, packageId => PackageId, req_id => Req_id, reason => Reason}),
+            {500, #{error => Reason}}
     end,
     {StatusCode, RespBody} = Response,
     Req2 = cowboy_req:reply(StatusCode, #{<<"content-type">> => <<"application/json">>}, jiffy:encode(RespBody), Req1),
@@ -29,6 +36,10 @@ init(Req=#{method := <<"POST">>}, State) ->
 
 
 init(Req0, State) ->
+    Req_id = maps:get(req_id, maps:get(package_tracker, Req0)),
+
+    lumberjack_server:warning("Invalid request method", #{module => ?MODULE, method => cowboy_req:method(Req0), peer_ip => cowboy_req:peer(Req0), req_id => Req_id}),
+
     Req1 = cowboy_req:reply(405, #{
         <<"allow">> => <<"POST">>
     }, Req0),
