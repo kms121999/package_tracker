62a5096d74bf9ae10c4f9fde5e957e7247e46ebb
Keaton Smith <keaton.smith365@gmail.com>
2024-11-20 10:37:22 -0700

    runnable

--- apps/lumberjack/src/lumberjack_rabbitmq_handler.erl
+++ apps/lumberjack/src/lumberjack_rabbitmq_handler.erl
@@ -3,5 +3,5 @@
 -behaviour(gen_event).
 
 %% API
--export([init/1, handle_event/2, handle_info/2, terminate/2, code_change/3]).
+% -export([init/1, handle_event/2, handle_info/2, terminate/2, code_change/3]).
 
