0b298bed69df97038c370f4f078b9581c26ff467
Keaton Smith <keaton.smith365@gmail.com>
2024-12-12 14:26:37 -0700

    push debug message

--- startup.sh
+++ startup.sh
@@ -4,5 +4,5 @@ if [ -z "$1" ]; then
     echo "Usage: you must enter the name for the node. "
     exit 1
 fi
-echo "Starting $1@$1.keatonsmith.com"
+echo "Starting $1$2@$1$2.keatonsmith.com"
 rebar3 shell --relname=$1 --name $1$2@$1$2.keatonsmith.com --setcookie package_tracker_cookie
